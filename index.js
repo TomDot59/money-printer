06b5499fe786af515fd92eae870de356f9c020c5
Thomas Ringot <Thomas_RINGOT@ext.connect-tech.sncf>
2023-03-01 14:34:07 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,134 @@
+// TODO: moduler, trigger notification
+require("dotenv").config();
+const needle = require("needle");
+const token = process.env.BEARER_TOKEN;
+
+const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
+const streamURL = "https://api.twitter.com/2/tweets/search/stream";
+
+const rules = [
+  {
+    value: "from:TomDot12",
+  },
+];
+
+async function getAllRules() {
+  const response = await needle("get", rulesURL, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (response.statusCode !== 200) {
+    console.log("Error:", response.statusMessage, response.statusCode);
+    throw new Error(response.body);
+  }
+
+  return response.body;
+}
+
+async function deleteAllRules(rules) {
+  if (!Array.isArray(rules.data)) {
+    return null;
+  }
+
+  const ids = rules.data.map((rule) => rule.id);
+
+  const data = {
+    delete: {
+      ids: ids,
+    },
+  };
+
+  const response = await needle("post", rulesURL, data, {
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (response.statusCode !== 200) {
+    throw new Error(response.body);
+  }
+
+  return response.body;
+}
+
+async function setRules() {
+  const data = {
+    add: rules,
+  };
+
+  const response = await needle("post", rulesURL, data, {
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (response.statusCode !== 201) {
+    console.dir(response.body);
+    throw new Error(response.body);
+  }
+
+  return response.body;
+}
+
+function streamConnect(retryAttempt) {
+  const stream = needle.get(streamURL, {
+    headers: {
+      "User-Agent": "v2FilterStreamJS",
+      Authorization: `Bearer ${token}`,
+    },
+    timeout: 20000,
+  });
+
+  console.log("stream started");
+
+  stream
+    .on("data", (data) => {
+      try {
+        const json = JSON.parse(data);
+        console.log(json);
+        retryAttempt = 0;
+      } catch (e) {
+        if (
+          data.detail ===
+          "This stream is currently at the maximum allowed connection limit."
+        ) {
+          console.log(data.detail);
+          process.exit(1);
+        } else {
+          console.log("listening...");
+        }
+      }
+    })
+    .on("err", (error) => {
+      if (error.code !== "ECONNRESET") {
+        console.log(error.code);
+        process.exit(1);
+      } else {
+        setTimeout(() => {
+          console.warn("A connection error occurred. Reconnecting...");
+          streamConnect(++retryAttempt);
+        }, 2 ** retryAttempt);
+      }
+    });
+
+  return stream;
+}
+
+(async () => {
+  let currentRules;
+
+  try {
+    currentRules = await getAllRules();
+    await deleteAllRules(currentRules);
+    await setRules();
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+
+  streamConnect(0);
+})();
